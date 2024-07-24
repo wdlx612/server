bcdedef96b57bde6242ba8e3b4b7875c31863814
dream-djaxtech <githubitsupport@djaxtech.com>
2024-07-24 07:04:10 +0000

    second

---
+++ var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2024-07-12 11:59:12
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
---
+++ var/templates_compiled/%%EC^ECD^ECDFE081%%passthrough.html.php
@@ -0,0 +1,3 @@
+<?php /* Smarty version 2.6.18, created on 2024-07-12 18:25:14
+         compiled from passthrough.html */ ?>
+<?php echo $this->_tpl_vars['content']; ?>
